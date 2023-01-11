024ec7db96bc6a8379d6430be872e57f05648b89
Skrinshotik <60069877+Skrinshotik@users.noreply.github.com>
2023-01-11 14:16:57 +0300

    Changed the implementation of the exchange
    Now the exchange is realized through the 3rd person, which we are.

--- TestBot.py
+++ TestBot.py
@@ -1,4 +1,4 @@
-﻿from logging import exception
+from logging import exception
 import pywaves as pw
 import telebot
 import ccxt
@@ -6,35 +6,30 @@ from telebot import types
 
 
 # Объявление глобальных переменных
-we = ccxt.wavesexchange({
-    'apiKey': 'PUBLIC KEY',
-    'secret': 'PRIVATE KEY',
-    'enableRateLimit': True,
-})
-bot = telebot.TeleBot('5808235694:AAE8_zfJJ1Jqh5JQQXQEkpXGd_g6bJXDjT0');
+assetId = "8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS"
+yourAddress = "3PEZUy4L4BQgn9B7jpQ7sQm4j9RgsqsW2RH"
+bot = telebot.TeleBot('BOT API')
 amount_in = 0
 price_in = 0
-side = '' 
 
 #Стартовая функция бота
 @bot.message_handler(content_types=['text'])
 def start(message):
     try:
         if message.text == '/start':
-            bot.send_message(message.chat.id, 'test bot - WavesExchange');
+            bot.send_message(message.chat.id, 'test bot - WavesExchange')
             callback_task(message)
         else:
-            bot.send_message(message.chat.id, 'write /start');
+            bot.send_message(message.chat.id, 'write /start')
     except Exception:
         bot.send_message(message.chat.id, 'Something went wrong  : (')        
         
 #Вывод сообщения с клавиатурой, для вызова определенных функций
 def callback_task(message):
-    keyboard = types.InlineKeyboardMarkup();
-    key_price = types.InlineKeyboardButton(text='BTC/USDT', callback_data= 'price');
-    key_buy_usdt= types.InlineKeyboardButton(text='BTC --> USDT', callback_data= "buy_usdt");
-    key_buy_btc= types.InlineKeyboardButton(text='USDT --> BTC', callback_data= "buy_btc");
-    keyboard.add(key_price, key_buy_usdt, key_buy_btc);
+    keyboard = types.InlineKeyboardMarkup()
+    key_price = types.InlineKeyboardButton(text='BTC/USDT', callback_data= 'price')
+    key_buy_usdt= types.InlineKeyboardButton(text='Exchange BTC', callback_data= "transfer_BTC")
+    keyboard.add(key_price, key_buy_usdt);
     bot.send_message(message.chat.id, 'Choose option', reply_markup=keyboard)
 
 #Обработка пользовательского ввода количества валюты
@@ -42,36 +37,26 @@ def callback_task(message):
 def amount_input(message):
     global amount_in
     amount_in = message.text
-    bot.send_message(message.chat.id, 'Enter price:');
-    bot.register_next_step_handler(message, price_input)
+    we.load_markets()
+    price = we.fetch_ticker('BTC/USDT')['last']
+    confirm_input = types.InlineKeyboardMarkup()
+    confirmation = types.InlineKeyboardButton(text='Confirm!', callback_data= 'confirm')
+    canceling = types.InlineKeyboardButton(text='Cancel', callback_data= 'cancel')
+    confirm_input.add(confirmation, canceling)
+    result = float(price)*float(amount_in)
+    bot.send_message(message.chat.id, f'It will be - {result} USDT\n', reply_markup=confirm_input)
 
-#Обработка пользовательского ввода цены валюты
 @bot.message_handler(content_types=['text'])
-def price_input(message):
-    global price_in
-    price_in = message.text    
-    transaction(message)
-
-#Совершение выбранной пользователем транзакции
-@bot.message_handler(content_types=['text'])
-def transaction(message):
-    we.load_markets()   
-
-    # пытался прописать обработку исключения, но постоянно выбивало новое исключение с ошибкой доступа по запросу к ресурсу, связанному с WavesX
-    #try:
-    #    order = we.createOrder('BTC/USDT', 'limit', side, amount_in, price_in)
-    #    bot.send_message(message.chat.id, 'Transaction was successful');
-    #except ccxt.InsufficientFunds:
-    #    bot.send_message(message.chat.id, 'Something went wrong  : (') 
-
-    try:
-        order = we.createOrder('BTC/USDT', 'limit', side, amount_in, price_in)['id']
-        bot.send_message(message.chat.id, 'Transaction was successful' + f'\nOrder id: {order}');
-    except Exception:
-        bot.send_message(message.chat.id, 'Something went wrong  : (') 
-    callback_task(message)
+def redirect(message):
+    global amount_in
+    trans_url = types.InlineKeyboardMarkup()
+    redirect_button = types.InlineKeyboardButton(text='Pay', url= f"https://waves.exchange/#send/{assetId}?recipient={yourAddress}&amount={amount_in}")
+    canceling = types.InlineKeyboardButton(text='Cancel', callback_data= 'cancel')
+    trans_url.add(redirect_button, canceling)
+    bot.send_message(message.chat.id, f'1. Follow the link for payment \n2. Log in \n3. Confirm the transaction\n', reply_markup=trans_url)
     
-#Обработка нажитий на кнопки из функции callback_task
+   
+#Обработка нажитий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     global side
@@ -80,21 +65,23 @@ def callback_worker(call):
     if call.data == "price":
         we.load_markets()
         price = we.fetch_ticker('BTC/USDT')['last']
-        bot.send_message(call.message.chat.id, f'Current price of BTC/USDT --> {price}');  
+        bot.send_message(call.message.chat.id, f'Current price of BTC/USDT --> {price}')
         callback_task(call.message)     
         
     #Покупка BTC/USDT
-    elif call.data == "buy_btc":
-        bot.send_message(call.message.chat.id, 'Enter amount:');
-        side = 'buy'
+    elif call.data == "transfer_BTC":
+        bot.send_message(call.message.chat.id, 'Enter amount:')
         bot.register_next_step_handler(call.message, amount_input)
+        
+    #Перенаправление на оплату
+    elif call.data == "confirm":
+        redirect(call.message)
 
-    #Продажа BTC/USDT
-    elif call.data == "buy_usdt":
-        bot.send_message(call.message.chat.id, 'Enter amount:');
-        side = 'sell'
-        bot.register_next_step_handler(call.message, amount_input)
+    #Отмена операции и перенаправление в главное меню
+    elif call.data == "cancel":
+        callback_task(call.message) 
 
         
 
 bot.polling(none_stop=True, interval=0)
+
